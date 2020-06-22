fc6a163d85f15a268600846f27c1161d65b06cdc
moohsin07 <moohsin68@gmail.com>
2020-06-23 01:33:26 +0200

    change on media query

--- js/main.js
+++ js/main.js
@@ -1,74 +1,120 @@
 $(document).ready(function () {
 
-    // Spinner Loading
+  // Spinner Loading
 
-    $(window).on("load", function () {
+  $(window).on("load", function () {
 
-        $(".loading-overlay").fadeOut();
+    $(".loading-overlay").fadeOut();
 
-        $("body").css("overflow", "auto")
+    $("body").css("overflow", "auto")
 
 
-    })
+  })
 
-    // change header height
+  // change header height
 
-    $('.header').height($(window).height());
+  $('.header').height($(window).height());
 
-    // adjust header-nav
+  // adjust header-nav
 
-    $('.nav-button').click(function () {
+  $('.nav-button').click(function () {
 
-        $(this).toggleClass('visable')
+    $(this).toggleClass('visable')
 
-        if ($(this).hasClass('visable')) {
+    if ($(this).hasClass('visable')) {
 
-            $('.line:first-child').css({
-                "transform": "rotate(45deg)",
-                "margin-bottom": "0"
-            })
+      $('.line:first-child').css({
+        "transform": "rotate(45deg)",
+        "margin-bottom": "0"
+      })
 
-            $('.line:last-child').css({
-                "transform": "rotate(-45deg)",
-                "margin-bottom": "0"
-            })
+      $('.line:last-child').css({
+        "transform": "rotate(-45deg)",
+        "margin-bottom": "0"
+      })
 
-            $('.header-nav').css({
-                "transform": "perspective(200px) rotateX(0deg) scale(1.0)",
-                "opacity": "1",
-                "visibility": "Visible"
+      $('.header-nav').css({
+        "transform": "perspective(200px) rotateX(0deg) scale(1.0)",
+        "opacity": "1",
+        "visibility": "Visible"
 
-            })
+      })
 
-        }
+    }
 
-        else {
+    else {
 
-            $('.line:first-child').css({
-                "transform": "rotate(0deg)",
-                "margin-bottom": "5px"
-            })
+      $('.line:first-child').css({
+        "transform": "rotate(0deg)",
+        "margin-bottom": "5px"
+      })
 
-            $('.line:last-child').css({
-                "transform": "rotate(0deg)",
-            })
+      $('.line:last-child').css({
+        "transform": "rotate(0deg)",
+      })
 
-            $('.header-nav').css({
-                "transform": "perspective(200px) rotateX(-15deg) scale(0.75)",
-                "opacity": "0",
-                "visibility": "hidden"
+      $('.header-nav').css({
+        "transform": "perspective(200px) rotateX(-15deg) scale(0.75)",
+        "opacity": "0",
+        "visibility": "hidden"
 
-            })
-        }
-    })
+      })
+    }
+  })
 
-    // navbar
+  // navbar
 
-    $('.navigation a , .nested-nav a , .header-title a').click(function (e) {
+  $('.navigation a , .nested-nav a , .header-title a').click(function (e) {
 
-        e.preventDefault();
+    e.preventDefault();
 
-        // change background and tittle
+    // change background and tittle
+
+    var bg = $(this).data('bg');
+
+    var text = $(this).data('text');
+
+    $(".bg").css("background-image", "url(" + bg + ")");
+
+    $('.fixed-title h1').text(text)
+
+    // smoth scroll to element
+
+    $('html,body').animate({
+
+      scrollTop: $('#' + $(this).data('scroll')).offset().top - 20
+
+    }, 1000)
+  });
+
+  // add active class on navbar
+
+  $('.navbar-nav li a').click(function () {
+
+    $(this).addClass('active').parent().siblings().find('a').removeClass('active')
+
+  })
+
+  // sync links & background with it's section
+
+  $(window).scroll(function () {
+
+    $('.block').each(function () {
+
+      if ($(window).scrollTop() > $(this).offset().top - 130) {
+
+        var blockID = $(this).attr('id');
+
+        $('.navigation a').removeClass('active')
+
+        $('.navigation a[data-scroll="' + blockID + '"]').addClass('active');
+
+      }
+    });
+
+    if ($('.navigation a').hasClass('active'))
+
+      $('.navigation a.active').each(function () {
 
         var bg = $(this).data('bg');
 
@@ -78,198 +124,152 @@ $(document).ready(function () {
 
         $('.fixed-title h1').text(text)
 
-        // smoth scroll to element
+      })
 
-        $('html,body').animate({
 
-            scrollTop: $('#' + $(this).data('scroll')).offset().top - 20
+  })
 
-        }, 1000)
+  // calling counter plugin
+
+  $('.timer').countTo()
+
+  // show the inner achivement
+  $(function () {
+
+    $('.achiv-inner').first().animate({
+      height: '100%'
     });
 
-    // add active class on navbar
+    $('.achiv-head').click(function () {
 
-    $('.navbar-nav li a').click(function () {
+      $(this).next().show().animate({ height: '100%' }, 600).siblings(".achiv-inner").animate({ height: 0 }, 500)
 
-        $(this).addClass('active').parent().siblings().find('a').removeClass('active')
-
-    })
-
-    // sync links & background with it's section
-
-    $(window).scroll(function () {
-
-        $('.block').each(function () {
-
-            if ($(window).scrollTop() > $(this).offset().top - 130) {
-
-                var blockID = $(this).attr('id');
-
-                $('.navigation a').removeClass('active')
-
-                $('.navigation a[data-scroll="' + blockID + '"]').addClass('active');
-
-            }
-        });
-
-        if ($('.navigation a').hasClass('active'))
-
-            $('.navigation a.active').each(function () {
-
-                var bg = $(this).data('bg');
-
-                var text = $(this).data('text');
-
-                $(".bg").css("background-image", "url(" + bg + ")");
-
-                $('.fixed-title h1').text(text)
-
-            })
-
-
-    })
-
-    // calling counter plugin
-
-    $('.timer').countTo()
-
-    // show the inner achivement
-    $(function () {
-
-        $('.achiv-inner').first().animate({
-            height: '180px'
-        });
-
-        $('.achiv-head').click(function () {
-
-            $(this).next().show().animate({ height: '180px' }, 600).siblings(".achiv-inner").animate({ height: 0 }, 500)
-
-        });
     });
+  });
 
-    // slider service
+  // slider service
 
-    var sliderItem = $('.slider-item').length
+  var sliderItem = $('.slider-item').length
 
-    var itemWidth = $('.slider-item').width()
+  var itemWidth = $('.slider-item').width()
 
-    $('.slider-stage').css("width", sliderItem * itemWidth + sliderItem * 18 + "px")
+  $('.slider-stage').css("width", sliderItem * itemWidth + sliderItem * 18 + "px")
 
-    $('.service .right').click(function () {
+  $('.service .right').click(function () {
 
-        $('.slider-stage').css("transform", "translateX( -" + itemWidth + "px)")
+    $('.slider-stage').css("transform", "translateX( -" + itemWidth + "px)")
 
-    })
+  })
 
-    $('.service .left').click(function () {
+  $('.service .left').click(function () {
 
-        $('.slider-stage').css("transform", "translateX(0px)")
+    $('.slider-stage').css("transform", "translateX(0px)")
 
-    })
+  })
 
-    // Modal Img
+  // Modal Img
 
-    var modal = $("#myModal"),
+  var modal = $("#myModal"),
 
-        modalImg = $("#img01"),
+    modalImg = $("#img01"),
 
-        icon = $(".inner-hover .fa-arrows-alt-v");
+    icon = $(".inner-hover .fa-arrows-alt-v");
 
-    icon.click(function () {
+  icon.click(function () {
 
-        modal.css({
+    modal.css({
 
-            "opacity": "1",
+      "opacity": "1",
 
-            "visibility": "visible"
-
-        });
-
-        modalImg.attr("src", $(this).data("src"));
+      "visibility": "visible"
 
     });
 
-    // close modal
+    modalImg.attr("src", $(this).data("src"));
 
-    modal.click(function () {
+  });
 
-        modal.css({
+  // close modal
 
-            "opacity": "0",
+  modal.click(function () {
 
-            "visibility": "hidden"
+    modal.css({
 
-        });
+      "opacity": "0",
+
+      "visibility": "hidden"
 
     });
 
-    // stopPropagation at modalImg
+  });
 
-    modalImg.click(function (e) {
+  // stopPropagation at modalImg
 
-        e.stopPropagation();
-    })
+  modalImg.click(function (e) {
 
-    // Animated Progress
+    e.stopPropagation();
+  })
 
-    $(window).scroll(function () {
+  // Animated Progress
 
-        if ($(window).scrollTop() >= $('.service').offset().top + 520) {
+  $(window).scroll(function () {
 
-            $('.progress span').each(function () {
+    if ($(window).scrollTop() >= $('.service').offset().top + 520) {
 
-                $(this).animate({
+      $('.progress span').each(function () {
 
-                    width: $(this).data('progress')
+        $(this).animate({
 
-                }, 1500)
+          width: $(this).data('progress')
 
-            })
+        }, 1500)
 
-            $('.skill-bar-title .perc').each(function () {
+      })
 
-                $(this).animate({
+      $('.skill-bar-title .perc').each(function () {
 
-                    left: $(this).data('perc')
+        $(this).animate({
 
-                }, 1500)
+          left: $(this).data('perc')
 
-            })
+        }, 1500)
 
-        }
+      })
 
-    })
+    }
 
-    // clints Slider
+  })
+
+  // clints Slider
 
 
-    var slide = $(".slide"),
+  var slide = $(".slide"),
 
-        slideWidth = slide.width(),
+    slideWidth = slide.width(),
 
-        i = 0;
+    i = 0;
 
-    $(".slides").css("width", slideWidth * slide.length)
+  $(".slides").css("width", slideWidth * slide.length)
 
-    $(".clints .right").click(function () {
+  $(".clints .right").click(function () {
 
-        if (i < slide.length - 1) i++
+    if (i < slide.length - 1) i++
 
-        else if (i == slide.length - 1) i = 0
+    else if (i == slide.length - 1) i = 0
 
-        slide.eq(0).css("margin-left", "-" + i * slideWidth + "px")
+    slide.eq(0).css("margin-left", "-" + i * slideWidth + "px")
 
-    })
+  })
 
-    $(".clints .left").click(function () {
+  $(".clints .left").click(function () {
 
-        if (i == 0) i = slide.length - 1
+    if (i == 0) i = slide.length - 1
 
-        else if (i > 0) i--
+    else if (i > 0) i--
 
-        slide.eq(0).css("margin-left", "-" + i * slideWidth + "px")
+    slide.eq(0).css("margin-left", "-" + i * slideWidth + "px")
 
-    })
+  })
 
 
 });
